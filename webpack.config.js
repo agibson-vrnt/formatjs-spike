7f990485e789ca6090666deef630441b35869e07
Andrew Gibson <andrew.gibson@verint.com>
2016-02-16 14:31:36 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/*eslint-env node*/
+
+var path = require( "path" );
+var webpack = require( "webpack" );
+
+module.exports = {
+
+    // your entry point - js/index.js or perhaps js/main.js - can be an array of files
+    entry: {
+
+        "bundle.js": path.resolve( __dirname, "js/inde.js" ),
+
+    },
+
+    // the output folder when forming the packed bundle (also affects the dev server's naming)
+    output: {
+
+        path: path.resolve( __dirname, "app/public/js/" ),
+        publicPath: "/public/js/",
+        filename: "[name].js",
+        libraryTarget: "umd"
+
+    },
+    module: {
+
+        "loaders": [
+            // this loader works for both js and jsx - react and es2015
+            {
+                test: /.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: [ "es2015", "react" ]
+                }
+            }
+
+        ]
+
+    },
+    plugins: [
+
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+               warnings: false
+            }
+        })
+
+    ]
+
+};
